27b491b8b7ccc3cef887d2597c6e4546a42a9f26
aimtbr <maxim.marchuk.d@gmail.com>
2020-11-15 01:59:03 +0200

    Update constants and helpers

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,7 +1,26 @@
-export const emailPattern = new RegExp(/^[^.,\s][\d,\w,\S]+@[\d,\w]+\.\w+$/);
+export const emailPattern = /^[^.,\s][\d,\w,\S]+@[\d,\w]+\.\w+$/;
 
 export const MAX_LOGIN_LENGTH = 25;
-export const MAX_EMAIL_LENGTH = 30;
+export const MAX_EMAIL_LENGTH = 52;
 export const MAX_PASSWORD_LENGTH = 100;
 export const MIN_PASSWORD_LENGTH = 8;
-export const DEFAULT_TEXT_INPUT_SIZE = 25;
+export const DEFAULT_FORM_INPUT_SIZE = 25;
+export const DEFAULT_TABLE_TEXT_INPUT_SIZE = 20;
+
+export const DEFAULT_CONTACTS = '+380';
+export const DEFAULT_GENDER = 'other';
+export const GENDERS_ENUM = [
+  'male',
+  'female',
+  'other'
+];
+
+export const DEFAULT_POSITION = 'n/n';
+export const POSITIONS_ENUM = [
+  'n/n',
+  'Developer',
+  'HR',
+  'CEO',
+  'Legal',
+  'Finance'
+];
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -5,15 +5,43 @@ const salt = process.env.AES_SALT;
 
 export const getCookie = (cookieName) => {
   const allCookies = document.cookie;
-  console.log(allCookies);
+
   if (allCookies === '') {
     return allCookies;
   }
 
   const arrayOfCookies = allCookies.split('; ');
-  const resultCookie = arrayOfCookies.filter((cookie) => cookie.startsWith(`${cookieName}`));
+  const resultCookie = arrayOfCookies.filter((cookie) => cookie.startsWith(cookieName));
 
   return resultCookie;
 };
 
-export const encryptAES = (data, newSalt = salt) => crypto.AES.encrypt(data, newSalt).toString()
+export const deleteCookie = (cookieName) => {
+  document.cookie = `${cookieName}=; max-age=-1`;
+};
+
+export const encryptAES = (data, newSalt = salt) => crypto.AES.encrypt(data, newSalt).toString();
+
+export const formatDate = (date) => {
+  let dateCopy = date;
+
+  if (!(dateCopy instanceof Date)) {
+    dateCopy = new Date(dateCopy);
+  }
+
+  return `${dateCopy.getDate()}.${dateCopy.getMonth() + 1}.${dateCopy.getFullYear()}`;
+};
+
+export const formatEmployeesDoc = (doc) => {
+  const { _id: id, dateAdded, ...rest } = doc;
+
+  const dateAddedWrapped = new Date(dateAdded);
+
+  const formattedDoc = {
+    ...rest,
+    id,
+    dateAdded: dateAddedWrapped,
+  };
+
+  return formattedDoc;
+};
