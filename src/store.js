47d2043b083916908e9f75be5eb0052d8da6fa62
aimtbr <maxim.marchuk.d@gmail.com>
2020-11-15 01:53:38 +0200

    Add the localforage package, whitelist the signUp and app reducers

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import storage from 'localForage';
 
 import rootReducer from './elements/reducers';
 
@@ -9,6 +9,7 @@ import rootReducer from './elements/reducers';
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['signUp', 'app']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
