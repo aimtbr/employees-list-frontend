d53bc95894d27a5575073893fd6dbd296899a5b6
aimtbr <maxim.marchuk.d@gmail.com>
2020-11-19 04:01:51 +0200

    Add stylesheets to Auth and SignUp pages

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -27,48 +27,57 @@ const Auth = (props) => {
     isFailed,
   } = props;
 
-  const body = (<div>
-    <div
-      id="auth-failed"
-      className={'invalid-field' + (isFailed ? ' shown' : '')}
-    >Invalid login or password</div>
+  const body = (<div id="auth">
+    <h1 id="page-title">Employees</h1>
 
-    <input
-      className="form-input"
-      type="text"
-      size={DEFAULT_FORM_INPUT_SIZE}
-      maxLength={MAX_LOGIN_LENGTH}
-      onChange={(event) => updateLogin(event.target.value)}
-      value={login}
-      autoComplete="username"
-      placeholder="Login"
-      required
-    />
+    <div id="auth-failed">
+      <span
+        className={'invalid-field' + (isFailed ? ' shown' : '')}
+      >Invalid login or password</span>
+    </div>
 
-    <input
-      className="form-input"
-      type="password"
-      size={DEFAULT_FORM_INPUT_SIZE}
-      minLength={MIN_PASSWORD_LENGTH}
-      maxLength={MAX_PASSWORD_LENGTH}
-      onChange={(event) => updatePassword(event.target.value)}
-      value={password}
-      autoComplete="current-password"
-      placeholder="Password"
-      required
-    />
+    <div className="form-input-wrapper">
+      <input
+        className="form-input"
+        type="text"
+        size={DEFAULT_FORM_INPUT_SIZE}
+        maxLength={MAX_LOGIN_LENGTH}
+        onChange={(event) => updateLogin(event.target.value)}
+        value={login}
+        autoComplete="username"
+        placeholder="Login"
+      />
+    </div>
 
-    <button
-      id="auth-login-btn"
-      onClick={(event) => {
-        event.preventDefault();
-        logInUser({ login, password }, history);
-      }}
-    >Log in</button>
+    <div className="form-input-wrapper">
+      <input
+        className="form-input"
+        type="password"
+        size={DEFAULT_FORM_INPUT_SIZE}
+        minLength={MIN_PASSWORD_LENGTH}
+        maxLength={MAX_PASSWORD_LENGTH}
+        onChange={(event) => updatePassword(event.target.value)}
+        value={password}
+        autoComplete="current-password"
+        placeholder="Password"
+      />
+    </div>
 
-    <span> / </span>
+    <div id="auth-buttons">
+      <button
+        id="auth-login-btn"
+        className="default-button"
+        type="button"
+        onClick={() => logInUser({ login, password }, history)}
+      >Log in</button>
 
-    <Link to={pages.signUp.path} id="auth-signup-btn">Sign up</Link>
+      <button
+        id="auth-signup-btn"
+        className="default-button"
+        type="button"
+        onClick={() => history.push(pages.signUp.path)}
+      >Sign up</button>
+    </div>
   </div>);
 
   return (
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -13,6 +13,8 @@ import {
 
 import pages from '..';
 
+import './styles.css';
+
 
 const SignUp = (props) => {
   const {
@@ -29,81 +31,99 @@ const SignUp = (props) => {
 
   const isAuthorized = user !== null;
 
-  const body = <div>
-    <label className="pre-field-label">
-      Email:
-      <input
-        className="form-input"
-        type="email"
-        size={DEFAULT_FORM_INPUT_SIZE}
-        maxLength={MAX_EMAIL_LENGTH}
-        pattern={emailPattern}
-        onChange={(event) => updateEmail(event.target.value)}
-        autoComplete="email"
-        value={email}
-        required
-      />
-    </label>
-    <div className={'invalid-field' + (isEmailInvalid ? ' shown' : '')}>
-      Email is already in use
+  const body = (<div id="signup">
+    <h1 id="page-title">Sign up</h1>
+
+    <div id="signup-form">
+      <div className="signup-form-group-wrapper">
+        <div className="signup-form-group">
+          <label className="prefield-label">
+            Email:
+          </label>
+          <input
+            className="form-input"
+            type="email"
+            size={DEFAULT_FORM_INPUT_SIZE}
+            maxLength={MAX_EMAIL_LENGTH}
+            pattern={emailPattern}
+            onChange={(event) => updateEmail(event.target.value)}
+            autoComplete="email"
+            value={email}
+            placeholder="your email"
+            required
+          />
+        </div>
+        <div className={'invalid-field' + (isEmailInvalid ? ' shown' : '')}>
+          Email is already in use
+        </div>
+      </div>
+
+      <div className="signup-form-group-wrapper">
+        <div className="signup-form-group">
+          <label className="prefield-label">
+            Login:
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            size={DEFAULT_FORM_INPUT_SIZE}
+            maxLength={MAX_LOGIN_LENGTH}
+            onChange={(event) => updateLogin(event.target.value)}
+            autoComplete="username"
+            value={login}
+            placeholder="new login"
+            required
+          />
+        </div>
+        <div className={'invalid-field' + (isLoginInvalid ? ' shown' : '')}>
+          Login is already in use
+        </div>
+      </div>
+
+      <div className="signup-form-group-wrapper">
+        <div className="signup-form-group">
+          <label className="prefield-label">
+            Password:
+          </label>
+          <input
+            className="form-input"
+            type="password"
+            size={DEFAULT_FORM_INPUT_SIZE}
+            minLength={MIN_PASSWORD_LENGTH}
+            maxLength={MAX_PASSWORD_LENGTH}
+            onChange={(event) => updatePassword(event.target.value)}
+            autoComplete="new-password"
+            value={password}
+            placeholder="new password"
+            required
+          />
+        </div>
+        <div className={'invalid-field' + (isPasswordInvalid ? ' shown' : '')}>
+          Invalid password
+        </div>
+      </div>
     </div>
 
-    <label className="pre-field-label">
-      Login:
-      <input
-        className="form-input"
-        type="text"
-        size={DEFAULT_FORM_INPUT_SIZE}
-        maxLength={MAX_LOGIN_LENGTH}
-        onChange={(event) => updateLogin(event.target.value)}
-        autoComplete="username"
-        value={login}
-        required
-      />
-    </label>
-    <div className={'invalid-field' + (isLoginInvalid ? ' shown' : '')}>
-      Login is already in use
+    <div id="signup-buttons">
+      <button
+        id="create-account-btn"
+        className="default-button"
+        type="button"
+        onClick={() => signUpUser({ email, login, password }, history)}
+      >Create account</button>
+
+      <button
+        id="back-btn"
+        className="default-button"
+        type="button"
+        onClick={() => history.replace(pages.auth.path)}
+      >Back</button>
     </div>
+  </div>);
 
-    <label className="pre-field-label">
-      Password:
-      <input
-        className="form-input"
-        type="password"
-        size={DEFAULT_FORM_INPUT_SIZE}
-        minLength={MIN_PASSWORD_LENGTH}
-        maxLength={MAX_PASSWORD_LENGTH}
-        onChange={(event) => updatePassword(event.target.value)}
-        autoComplete="new-password"
-        value={password}
-        required
-      />
-    </label>
-    <div className={'invalid-field' + (isPasswordInvalid ? ' shown' : '')}>
-      Invalid password
-    </div>
-
-    <button
-      className="signup-btn"
-      onClick={(event) => {
-        event.preventDefault();
-
-        signUpUser({ email, login, password }, history);
-      }}>Sign up</button>
-
-    <button
-      id="go-back-btn"
-      type="button"
-      onClick={() => history.replace(pages.auth.path)}
-    >Back</button>
-  </div>;
-
-  return (
-    <div>
-      {isAuthorized
-        ? <Redirect to={pages.list.path} />
-        : body}
-    </div>
+  return (isAuthorized
+    ? <Redirect to={pages.list.path} />
+    : body
   );
 };
 
